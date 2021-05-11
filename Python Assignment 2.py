282c6fa206910c0f961e7fef273786e18f35384f
cpandagare <83979787+cpandagare@users.noreply.github.com>
2021-05-11 12:38:43 +0530

    Add files via upload

---
+++ Python Assignment 2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+## Patter printing
+
+for i in range (1,5):
+    print('* '*(i))
+for i in range(5,0,-1):
+    print('* '*i)  
+
+
+# In[2]:
+
+
+## Reverse a word
+
+d= input('Enter a string to reverse: ')
+d[::-1]
+
+
+# In[ ]:
+
+
+
+
